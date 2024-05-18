d29fdb97de6cb8c9a9076730486760bdb55e3d03
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-05-18 22:36:13 +0200

    Make starter components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Graph from "./components/Graph";
+import Settings from "./components/Settings";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>Test</>;
+  const [array, setArray] = useState([]);
+  return (
+    <>
+      <Settings />
+      <Graph />
+    </>
+  );
 }
 
 export default App;
