b7e9ac32216f6bab14c7947995877ad310dd1893
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-05-19 17:02:44 +0200

    Style settings

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,21 @@ import { generateRandomArray } from "./utils/helpers";
 import "./App.css";
 
 function App() {
+  const [size, setSize] = useState(20);
+  const [speed, setSpeed] = useState(1);
+  const [algorithm, setAlgorithm] = useState("bubbleSort");
   const [array, setArray] = useState(() => generateRandomArray((length = 20)));
-
+  console.log(size, speed, algorithm);
   return (
     <div className="page">
-      <Settings />
+      <Settings
+        size={size}
+        speed={speed}
+        algorithm={algorithm}
+        setSize={setSize}
+        setSpeed={setSpeed}
+        setAlgorithm={setAlgorithm}
+      />
       <Graph array={array} />
     </div>
   );
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,5 +1,47 @@
-function Settings() {
-  return <div></div>;
+import "./Settings.css";
+
+function Settings({ size, speed, algorithm, setSize, setSpeed, setAlgorithm }) {
+  return (
+    <div className="settings">
+      <button>Sort</button>
+      <button>Generate New Array</button>
+      <div>
+        <select
+          onChange={(e) => setAlgorithm(e.target.value)}
+          value={algorithm}
+          defaultValue={"bubbleSort"}
+        >
+          <option value="bubbleSort">Bubble Sort</option>
+          <option value="selectionSort">Selection Sort</option>
+          <option value="insertionSort">Insertion Sort</option>
+        </select>
+      </div>
+      <div>
+        <label>Choose array size:</label>
+        <select
+          onChange={(e) => setSize(e.target.value)}
+          value={size}
+          defaultValue={"20"}
+        >
+          <option value="10">20</option>
+          <option value="20">30</option>
+          <option value="30">40</option>
+        </select>
+      </div>
+      <div>
+        <label>Choose speed:</label>
+        <select
+          onChange={(e) => setSpeed(e.target.value)}
+          value={speed}
+          defaultValue={"1"}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default Settings;
