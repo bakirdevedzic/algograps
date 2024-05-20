c8ecd48b1189e4d67626428e142244f4152e2391
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-05-20 10:22:45 +0200

    Add delay function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,6 +2,10 @@ export function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-export function generateRandomArray(length = 20, min = 10, max = 300) {
+export function generateRandomArray(length = 20, min = 10, max = 100) {
   return Array.from({ length }, () => getRandomNumber(min, max));
+}
+
+export function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
