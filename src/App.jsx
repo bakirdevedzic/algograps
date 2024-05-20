145a112ee016be650733fcd43a1a43a9dbf3b99b
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-05-20 10:24:15 +0200

    fix app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Graph from "./components/Graph/Graph";
 import Settings from "./components/Settings/Settings";
 import { generateRandomArray } from "./utils/helpers";
 import "./App.css";
 
+import { insertionSort } from "./utils/insertionSort";
+import { bubbleSort } from "./utils/bubbleSort";
+import { selectionSort } from "./utils/selectionSort";
+
 function App() {
+  const [isSorting, setIsSorting] = useState(false);
   const [size, setSize] = useState(20);
   const [speed, setSpeed] = useState(1);
   const [algorithm, setAlgorithm] = useState("bubbleSort");
-  const [array, setArray] = useState(() => generateRandomArray((length = 20)));
-  console.log(size, speed, algorithm);
+  const [compared, setCompared] = useState([]);
+  const [swap, setSwap] = useState([]);
+  const [array, setArray] = useState();
+
+  useEffect(() => {
+    setArray(generateRandomArray(Number(size)));
+  }, [size]);
+
+  const handleSort = () => {
+    if (algorithm === "selectionSort") {
+      selectionSort(
+        array,
+        setArray,
+        Number(speed) * 50,
+        setCompared,
+        setSwap,
+        setIsSorting
+      );
+    }
+    if (algorithm === "bubbleSort") {
+      bubbleSort(
+        array,
+        setArray,
+        Number(speed) * 50,
+        setCompared,
+        setSwap,
+        setIsSorting
+      );
+    }
+    if (algorithm === "insertionSort") {
+      insertionSort(
+        array,
+        setArray,
+        Number(speed) * 50,
+        setCompared,
+        setSwap,
+        setIsSorting
+      );
+    }
+    setIsSorting(true);
+  };
   return (
     <div className="page">
       <Settings
@@ -19,8 +63,10 @@ function App() {
         setSize={setSize}
         setSpeed={setSpeed}
         setAlgorithm={setAlgorithm}
+        handleSort={handleSort}
+        isSorting={isSorting}
       />
-      <Graph array={array} />
+      <Graph array={array} compared={compared} swap={swap} />
     </div>
   );
 }
